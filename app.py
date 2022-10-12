6f417d2d0aca4b24617258c2ded12ad73df81a0f
rocco1967 <gianfranco.fa@gmail.com>
2022-10-12 09:52:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -89,7 +89,7 @@ if st.button('Calcolo Calorie'):
     Calorie_Bruciate = predict(Gender, Age, Height, Weight, Duration, Heart_Rate,Body_Temp)
     #st.subheader(f' calcolo Kcalorie ... tolleranza... 5% ..   {Calorie_Bruciate[0]:.2f} KCAL')   #### originale ###
     st.subheader(f' calcolo Kcalorie ... tolleranza... 5% ..   {Calorie_Bruciate[0]:.2f} KCAL') 
-    st.header(f' calcolo Kcalorie....
+    #st.header(f' calcolo Kcalorie....
     #if Calorie_Bruciate<=300:
        #st.header('.........SFORZATI UN PO^ DI PIU^')
        #st.header('QUESTI LI VEDI SOLO a NATALE')
