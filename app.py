43f8be8ca8495c72a1442909909966aec1c43904
rocco1967 <gianfranco.fa@gmail.com>
2022-11-10 14:25:23 +0100

    Update app.py

--- app.py
+++ app.py
@@ -87,7 +87,7 @@ st.markdown(
 #tolleranza=(Calorie_Bruciate*3)/100
 if st.button('Calcolo Calorie'):
     Calorie_Bruciate = predict(Gender, Age, Height, Weight, Duration, Heart_Rate,Body_Temp)
-    tolleranza=(Calorie_Bruciate*3)/100
+    tolleranza=(Calorie_Bruciate*5)/100
     #st.subheader(f' calcolo Kcalorie ... tolleranza... 5% ..   {Calorie_Bruciate[0]:.2f} KCAL')   #### originale ###
     st.subheader(f' calcolo Calorie .....   {Calorie_Bruciate[0]:.2f} KCAL') 
     st.subheader(f' con tolleranza.....di... {tolleranza[0]:.0f} KCAL')  
